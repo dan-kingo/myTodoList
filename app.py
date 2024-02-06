88f19e79fffaabf175431cc1ebe56b3ae42e9588
Dan Kingo <danieldejen23@gmail.com>
2024-02-06 10:22:42 -0800

    finalize project and fix bug

--- app.py
+++ app.py
@@ -1,29 +1,31 @@
 import PySimpleGUI as pyGUI
-
+import time
 import utils
 
 input_label = pyGUI.Text("Write in a todo: ")
-input_box = pyGUI.InputText(tooltip="Type a todo",
-                            key="todo")
+input_box = pyGUI.InputText(tooltip="Type a todo",key="todo")
+
+time_label = pyGUI.Text('', key="clock")
+
 list_box = pyGUI.Listbox(values=utils.get_todos(),
                          key='todos',
-                         size=(50, 10),
+                         size=(56, 10),
                          enable_events=True)
 
-edit_button = pyGUI.Button("Edit")
-delete_button = pyGUI.Button("Delete")
-exit_button = pyGUI.Button("Exit")
-add_button = pyGUI.Button("Add")
+edit_button = pyGUI.Button("Edit", size=10)
+delete_button = pyGUI.Button("Delete", size=10)
+exit_button = pyGUI.Button("Exit", size=10)
+add_button = pyGUI.Button("Add", size=10)
 
 app = pyGUI.Window("My Todo App",
-                   layout=[[input_label],
+                   layout=[[time_label], [input_label],
                            [input_box, add_button],
                            [list_box],
                            [edit_button, delete_button, exit_button]],
                    font=("Montserrat", 18))
 while True:
-    event, values = app.read()
-
+    event, values = app.read(timeout=200)
+    app['clock'].update(time.strftime("%b %d, %Y at %I:%M:%S %p"))
     match event:
         case "Add":
             todos = utils.get_todos()
@@ -33,22 +35,32 @@ while True:
             app['todos'].update(values=todos)
 
         case "Edit":
-            todo_to_edit = values['todos'][0]
-            new_todo = values['todo']
-            todos = utils.get_todos()
-            index = todos.index(todo_to_edit)
-            todos[index] = new_todo
-            utils.set_todos(todos)
-            app['todos'].update(values=todos)
-
+            try:
+                todo_to_edit = values['todos'][0]
+                new_todo = values['todo']
+                todos = utils.get_todos()
+                index = todos.index(todo_to_edit)
+                todos[index] = new_todo
+                utils.set_todos(todos)
+                app['todos'].update(values=todos)
+            except IndexError:
+                pyGUI.popup("Please select an item first!",
+                            font=("Montserrat", 18),
+                            title="Information")
         case "Delete":
-            todo_to_delete = values['todos'][0]
-            todos = utils.get_todos()
-            todos.remove(todo_to_delete)
-            utils.set_todos(todos)
+            try:
+                todo_to_delete = values['todos'][0]
+                todos = utils.get_todos()
+                todos.remove(todo_to_delete)
+                utils.set_todos(todos)
 
-            app['todos'].update(values=todos)
-            app['todo'].update(value='')
+                app['todos'].update(values=todos)
+                app['todo'].update(value='')
+
+            except IndexError:
+                pyGUI.popup("Please select an item first!",
+                            font=("Montserrat", 18),
+                            title="Information")
 
         case "Exit":
             break
